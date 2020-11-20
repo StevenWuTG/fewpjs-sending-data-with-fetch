611c5c12caae5b5b2959b589f5824def61f1f567
StevenWuTG <stevenwu91@icloud.com>
2020-11-19 21:57:38 -0500

    deliverable 1 done brb

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your code here
+function submitData( name, email){
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+        addToDom(data)
+    })
+    .catch(error => {
+        addErrorToDom(error)
+    })
+}
