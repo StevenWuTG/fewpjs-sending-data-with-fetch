23015d2cb1182fb1f93623705da7aec11bb66ff5
StevenWuTG <stevenwu91@icloud.com>
2020-11-22 19:45:39 -0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // Add your code here
 function submitData( name, email){
 
-    return fetch("http://localhost:3000/users", configObj)
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                name: name,
+                email: email
+            }
+        )
+    })
     .then(response => response.json())
     .then(data => {
-        addToDom(data)
+        document.body.innerHTML = data["id"]
+        console.log(data)
     })
     .catch(error => {
-        addErrorToDom(error)
+        document.body.innerHTML = error.message
     })
 }
